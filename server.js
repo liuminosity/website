0f6b1c00b843e594e3d2c3e6a5ee590f7fb5b978
Liuminosity <liu.stanford@gmail.com>
2015-08-27 12:14:01 -0700

    v.32 added index path

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require('express')
 var app = express()
 var partials = require('express-partials');
+var path    = require("path");
 
 
-app.configure(function() {
-  app.set('port', (process.env.PORT || 5000))
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(partials());
-  app.use(express.static(__dirname + '/public'));
-});
+app.set('port', (process.env.PORT || 5000))
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response) {
-  response.sendFile('/index.html')
+  response.sendFile(path.join(__dirname+'/index.html'))
 })
 
 app.listen(app.get('port'), function() {
